29010f8aa38a783eebaf917fa27ce63497ca8eb0
mfclarke <maximilianfc@gmail.com>
2015-08-24 22:00:20 +0200

    Moved to XCTest measureBlock()

--- BenchmarkingTests/BenchmarkingTests.swift
+++ BenchmarkingTests/BenchmarkingTests.swift
@@ -9,28 +9,126 @@
 import UIKit
 import XCTest
 
+let nsDict = NSDictionary(
+    object: NSDictionary(
+        object: NSDictionary(
+            object: NSDictionary(
+                object: "Die Harder.",
+                forKey: "TagLine"),
+            forKey: "Die Hard 2"),
+        forKey: "Movies"),
+    forKey: "MediaTypes"
+)
+
+let swDict = ["MediaTypes": ["Movies": ["Die Hard 2": ["TagLine": "Die Harder."]]]]
+
 class BenchmarkingTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    let repeats = 10000
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
+    func testSwiftCastingIfLet() {
         self.measureBlock() {
-            // Put the code you want to measure the time of here.
+            for _ in 0...self.repeats {
+                if let theMediaTypes = nsDict["MediaTypes"] as? [String: AnyObject],
+                    theMovies = theMediaTypes["Movies"] as? [String: AnyObject],
+                    metadata = theMovies["Die Hard 2"] as? [String: AnyObject],
+                    tagline = metadata["TagLine"] as? String {
+                        tagline
+                } else {
+                    XCTFail("Value not fetched")
+                }
+            }
         }
     }
     
+    func testNSDictionaryIfLet() {
+        self.measureBlock() {
+            for _ in 0...self.repeats {
+                if let theMediaTypes = nsDict["MediaTypes"] as? NSDictionary,
+                    theMovies = theMediaTypes["Movies"] as? NSDictionary,
+                    metadata = theMovies["Die Hard 2"] as? NSDictionary,
+                    tagline = metadata["TagLine"] as? String {
+                        tagline
+                } else {
+                    XCTFail("Value not fetched")
+                }
+            }
+        }
+    }
+    
+    func testSwiftIfLet() {
+        self.measureBlock() {
+            for _ in 0...self.repeats {
+                if let theMediaTypes = swDict["MediaTypes"],
+                    theMovies = theMediaTypes["Movies"],
+                    metadata = theMovies["Die Hard 2"],
+                    tagline = metadata["TagLine"] {
+                        tagline
+                } else {
+                    XCTFail("Value not fetched")
+                }
+            }
+        }
+    }
+    
+    func testNSDictionaryOptionalChain() {
+        self.measureBlock() {
+            for _ in 0...self.repeats {
+                if let value = ((((nsDict["MediaTypes"] as? NSDictionary)?["Movies"] as? NSDictionary)?["Die Hard 2"] as? NSDictionary)?["TagLine"]) as? String {
+                    value
+                } else {
+                    XCTFail("Value not fetched")
+                }
+            }
+        }
+    }
+    
+    func testSwiftOptionalChain() {
+        self.measureBlock() {
+            for _ in 0...self.repeats {
+                if let value = swDict["MediaTypes"]?["Movies"]?["Die Hard 2"]?["TagLine"] {
+                    value
+                } else {
+                    XCTFail("Value not fetched")
+                }
+            }
+        }
+    }
+    
+    func testNSDictionaryValueForKeyPath() {
+        self.measureBlock() {
+            for _ in 0...self.repeats {
+                if let value = nsDict.valueForKeyPath("MediaTypes.Movies.Die Hard 2.TagLine") as? String {
+                    value
+                } else {
+                    XCTFail("Value not fetched")
+                }
+            }
+        }
+    }
+    
+    func testShallowNSDictionaryValue() {
+        self.measureBlock() {
+            for _ in 0...self.repeats {
+                if let value: AnyObject? = nsDict["MediaTypes"] {
+                    value
+                } else {
+                    XCTFail("Value not fetched")
+                }
+            }
+        }
+    }
+    
+    func testShallowSwiftDictionaryValue() {
+        self.measureBlock() {
+            for _ in 0...self.repeats {
+                if let value = swDict["MediaTypes"] {
+                    value
+                } else {
+                    XCTFail("Value not fetched")
+                }
+            }
+        }
+    }
+
 }
